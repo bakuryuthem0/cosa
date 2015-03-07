88beb988f819eec138d79c79432b28cfe7720f98
Rubenazo <rubenx_360@hotmail.com>
2015-03-07 16:27:43 -0430

    Initial commit

---
+++ app/models/Usuarios.php
@@ -0,0 +1,33 @@
+<?php
+class Usuarios extends Eloquent {
+	protected $table    = 'usuarios';
+	protected $fillable = array('nombre','apellido','correo','zona','plan_id');
+
+	public static function nuevoUsuario(input) {
+		$respuesta = array();
+
+		$reglas = array(
+			'nombre'   => 'required',
+			'apellido' => 'required',
+			'correo'   => array('required','email'),
+			'zona'     => 'required'
+		);
+
+		$validator = Validator::make($input,$reglas);
+
+		if ($validator->fails()) {
+			$respuesta['mensaje'] = $validator;
+			$respuesta['error']   = true;
+		}
+		else {
+			$usuarios = static::create($input)
+
+			$respuesta['mensaje'] = 'Usuario creado!';
+			$respuesta['error']   = false;
+			$respuesta['data']    = $usuarios;
+		}
+
+		return $respuesta;
+	} 
+}
+?>
---
+++ app/views/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Crear Usuario</title>
+		{{HTML::style("css/bootstrap.min.css")}}
+    	{{HTML::style("css/jumbotron-narrow.css")}}
+	</head>
+
+	<body>
+		<div class="container">
+			
+			<div clas="header">
+				<ul class="nav nav-pills pull-right">
+					<li>{{ HTML::link('/','Inicio') }}</li>
+					<li>{{ HTML::link('crear','Crear Usuario') }}</li>
+					<li>{{ HTML::link('','Mostrar Usuarios') }}</li>
+				</ul>
+				<h3 class="text-muted">Cosa</h3>
+			</div>
+
+			@yield('contenido')
+
+			<div class="footer">
+				<h3 class="text-muted text-right">Cosa</h3>
+			<div>
+
+		</div>
+	</body>
+</html>
